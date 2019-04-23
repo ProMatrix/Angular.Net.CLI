471a0f6760a04fa9d7442ce36c29e02dfea97e36
ddonovan <info@promatrixinc.com>
2019-04-23 18:41:28 -0400

    WIP

--- AngularDotNet/wwwroot/src/app/app.component.ts
+++ AngularDotNet/wwwroot/src/app/app.component.ts
@@ -1,10 +1,182 @@
-import { Component } from '@angular/core';
+import { ViewChild } from "@angular/core";
+import { Component } from "@angular/core";
+import { Router, ActivatedRoute, NavigationEnd } from "@angular/router";
+import * as moment from "moment";
+import { ToastrService } from 'ngx-toastr';
+import { filter } from 'rxjs/operators';
+// services
+import { AppConfig } from "../../common/appConfig";
+import { MessagePump } from "../../common/messagePump";
+import { AppServices } from "../../shared/ng2-apphelper/appServices";
+import { ModalDialog } from "../../shared/ng2-animation/modalDialog";
 
 @Component({
-  selector: 'app-root',
-  template: '\n<!--The content below is only a placeholder and can be replaced.-->\n<div style=\"text-align:center\">\n  <h1>\n    Welcome to {{ title }}!\n  </h1>\n  <img width=\"300\" alt=\"Angular Logo\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==\">\n</div>\n<h2>Here are some links to help you start: </h2>\n<ul>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://angular.io/tutorial\">Tour of Heroes</a></h2>\n  </li>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://angular.io/cli\">CLI Documentation</a></h2>\n  </li>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://blog.angular.io/\">Angular blog</a></h2>\n  </li>\n</ul>\n\n'/* this was squashed */,
-  styles: ['\n'/* this was squashed */]
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
+    // #endregion
+    providers: [AppConfig, AppServices, MessagePump]
 })
 export class AppComponent {
-  title = 'AngularDotNet';
+    @ViewChild(ModalDialog) md: ModalDialog;
+    private appTitle = "Angular.Net Starter Application";
+    private date: Date;
+    private theWeekOf: string;
+    private appHref: string;
+    private titleBlinking = true;
+    private titleVisibleWhenNotBlinking = true;
+    private showModalDialog = false;
+    private selectedFeature: string;
+    private appLoaded = false;
+    private resizeTimerId: any;
+    private subtitle = "";
+
+    constructor(private readonly route: ActivatedRoute, private readonly router: Router, private readonly ac: AppConfig, private readonly toastr: ToastrService, private readonly as: AppServices) {
+    }
+
+    private ngOnInit() {
+        this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd)
+        ).subscribe(event => {
+            let currentRoute = this.route.root;
+            while (currentRoute.children[0] !== undefined) {
+                currentRoute = currentRoute.children[0];
+            }
+            this.subtitle = currentRoute.snapshot.data.subtitle;
+        });
+
+        this.date = new Date();
+        this.theWeekOf = moment().startOf("week").format("ddd MMM D YYYY");
+        this.appHref = window.location.href;
+        this.ac.getAppSettings(() => {
+            this.checkForUpdates();
+            this.navigateForward();
+        }, (errorMessage) => {
+            if (navigator.onLine)
+                this.toastr.error(errorMessage);
+            else
+                this.toastr.warning(this.appTitle + ": is Offline!");
+            this.navigateForward();
+        });
+    }
+
+    private ngAfterViewInit() {
+        this.ac.onResizeApp();
+        this.ac.onOrientationChange();
+
+        window.addEventListener("offline", (event: Event) => {
+            this.toastr.info("The application just went offline!");
+            this.ac.isOnline = false;
+        }, false);
+
+        window.addEventListener("online", (event: Event) => {
+            this.toastr.info("The application is back online!");
+            this.ac.isOnline = true;
+        }, false);
+
+        window.addEventListener("resize", (event: Event) => {
+            if (this.resizeTimerId)
+                return;
+            this.resizeTimerId = setTimeout(() => {
+                this.ac.onResizeApp();
+                this.resizeTimerId = null;
+            }, 500);
+        }, false);
+
+        window.addEventListener("orientationchange", (event: Event) => {
+            setTimeout(() => {
+                this.ac.onOrientationChange();
+            });
+        }, false);
+    }
+
+    private updateVersionAndRestart() {
+        this.ac.setLocalStorage("versionNumber", { vn: this.ac.appSettings.projectVersionNo });
+        this.toastr.info("Updating to latest version! Restarting the application...");
+        setTimeout(() => {
+            this.restartApp();
+        }, 3000);
+    }
+
+    private checkForUpdates() {
+        if (this.ac.appSettings.debug)
+            return;
+
+        const versionNumber = this.ac.getLocalStorage("versionNumber");
+        if (!versionNumber)
+            this.updateVersionAndRestart();
+
+        if (versionNumber.vn !== this.ac.appSettings.projectVersionNo)
+            this.updateVersionAndRestart();
+
+        if (navigator.onLine) {
+            this.ac.isOnline = true;
+            this.toastr.success("This application is operating online as normal.", "Success!");
+        }
+        else {
+            this.ac.isOnline = false;
+            this.toastr.warning("This application is operating offline as normal.", "Warning!");
+        }
+    }
+
+    private getOnlineStatus() {
+        return navigator.onLine;
+    }
+
+    private navigateForward() {
+        setTimeout(() => {
+            if (window.screen.width <= 768) {
+                this.navigateTo("/mobileApis");
+                return;
+            }
+
+            const navigateTo = this.ac.getLocalStorage("navigateTo");
+            if (navigateTo)
+                this.navigateTo(navigateTo.feature);
+            else
+                this.navigateTo("/splash");
+            this.titleBlinking = false;
+            this.appLoaded = true;
+        }, this.ac.appSettings.splashTime); // navigate away from splash view        
+    }
+
+    private restartApp() {
+        window.location.href = this.appHref;
+    }
+
+    private currentRouteUrl(): string {
+        if (this.selectedFeature === "/restart")
+            return "/restart";
+        return this.router.url;
+    }
+
+    private navigateTo(feature) {
+        this.selectedFeature = feature;
+        if (feature === "/restart") {
+            setTimeout(() => {
+                this.restartApp();
+            }, 1000);
+            return;
+        }
+
+        this.ac.setLocalStorage("navigateTo", { feature: feature });
+        this.router.navigate([feature]);
+    }
+
+    private onClickAbout() {
+        this.md.modalDialogTitle = `About: ${this.appTitle}`;
+        this.md.showOkButton = true;
+        this.md.isClosable = true;
+        this.md.desiredWidth = 430;
+        this.md.desiredHeight = 300;
+        this.showModalDialog = false;
+        setTimeout(() => {
+            this.showModalDialog = true;
+        });
+        this.md.dialogButtonCallback = (buttonClicked: string) => {
+            if (buttonClicked === "ok") {
+                this.md.closeDialog();
+            }
+        };
+    }
 }
