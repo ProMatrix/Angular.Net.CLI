720754afc85eadd30a7e3b48d8b8b20faee1159b
ddonovan <info@promatrixinc.com>
2019-04-25 20:02:31 -0400

    ...

--- AngularDotNet/wwwroot/src/app/app.module.ts
+++ AngularDotNet/wwwroot/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
+import { StoreModule } from "@ngrx/store";
 
 import { AppAnimation } from "../../shared/ng2-animation/appAnimation";
 import { AppMobileTech } from "../../shared/ng2-mobiletech/appMobileTech";
@@ -16,43 +17,46 @@ import { AlreadyReady } from "../../features/alreadyReady";
 import { Analytics } from "../../features/analytics";
 import { Features } from "../../features/features";
 import { MobileApis } from "../../features/mobileApis";
-import { Notification } from "../../features/notification";
+import { NotificationComponent } from "../../features/notification/notification.component";
 import { Settings } from "../../features/settings";
 import { Splash } from "../../features/splash";
+import { NotificationModule } from '../../features/notification/notification.module';
 
 @NgModule({
-    declarations: [
-        AppComponent, AlreadyReady, Analytics, Features, Notification, MobileApis, Settings, Splash
-    ],
-    imports: [
-        BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-        HttpClientModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        AppAnimation,
-        AppMobileTech,
-        AppHelper.forRoot(),
+  declarations: [
+    AppComponent, AlreadyReady, Analytics, Features, NotificationComponent, MobileApis, Settings, Splash
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppAnimation,
+    AppMobileTech,
+    AppHelper.forRoot(),
 
-        RouterModule.forRoot([
-            { path: "", component: Splash, data: { subtitle: "Quick SPLASH" } },
-            { path: "splash", component: Splash, data: { subtitle: "Quick SPLASH" } },
-            { path: "settings", component: Settings, data: { subtitle: "VERSIONS & SETTINGS" } },
-            { path: "analytics", component: Analytics, data: { subtitle: "Application Analytics" } },
-            { path: "features", component: Features, data: { subtitle: "More About this Application" } },
-            { path: "notification", component: Notification, data: { subtitle: "Immediate Notification" } },
-            { path: 'alreadyReady', component: AlreadyReady, data: { subtitle: "Feature Quick Start" } },
-            { path: "mobileApis", component: MobileApis, data: { subtitle: "Modern Mobile Features" } },
-            { path: "**", redirectTo: "/splash", pathMatch: "full" }
-        ]),
-        ToastrModule.forRoot(
-            {
-                timeOut: 5000,
-                positionClass: 'toast-bottom-right',
-                preventDuplicates: true,
-            }
-        )
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+    RouterModule.forRoot([
+      { path: "", component: Splash, data: { subtitle: "Quick SPLASH" } },
+      { path: "splash", component: Splash, data: { subtitle: "Quick SPLASH" } },
+      { path: "settings", component: Settings, data: { subtitle: "VERSIONS & SETTINGS" } },
+      { path: "analytics", component: Analytics, data: { subtitle: "Application Analytics" } },
+      { path: "features", component: Features, data: { subtitle: "More About this Application" } },
+      { path: "notification", component: NotificationComponent, data: { subtitle: "Immediate Notification" } },
+      { path: 'alreadyReady', component: AlreadyReady, data: { subtitle: "Feature Quick Start" } },
+      { path: "mobileApis", component: MobileApis, data: { subtitle: "Modern Mobile Features" } },
+      { path: "**", redirectTo: "/splash", pathMatch: "full" }
+    ]),
+    ToastrModule.forRoot(
+      {
+        timeOut: 5000,
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: true,
+      }
+    ),
+    StoreModule.forRoot({}),
+    NotificationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ProjectBuild/wwwroot/shared/client-side-models/apiVersions.js
+++ ProjectBuild/wwwroot/shared/client-side-models/apiVersions.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 var ApiVersions = /** @class */ (function () {
     function ApiVersions() {
-        this.application = "1.0.00";
+        this.application = "1.0.4";
         this.typeScript = "3.2.2";
         this.webpackCLI = "";
         this.nodeJs = "8.12.0";
