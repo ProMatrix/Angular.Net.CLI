b7ecff9b61a27af7db77182869659635a2a188f5
ddonovan <info@promatrixinc.com>
2019-05-15 09:08:22 -0400

    ...

--- AngularDotNet/wwwroot/src/app/toolbar.component.ts
+++ AngularDotNet/wwwroot/src/app/toolbar.component.ts
@@ -1,30 +1,11 @@
-//import { Component, OnInit } from '@angular/core';
-
-//@Component({
-//  selector: 'app-toolbar',
-//  templateUrl: "./toolbar.component.html",
-//  styles: ['toolbar.component.css']
-//})
-//export class ToolbarComponent implements OnInit {
-
-//  constructor() { }
-
-//  ngOnInit() {
-//  }
-
-//}
-
-
-
-
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog, MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material';
-//import { NewContactDialogComponent } from '../new-contact-dialog/new-contact-dialog.component';
+
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
-  template: "\n<mat-toolbar color=\"primary\">\n  <button mat-button class=\"sidenav-toggle\" (click)=\"toggleSidenav.emit()\">\n    <mat-icon>menu</mat-icon>\n  </button>\n  <span>Angular.Net</span>\n  <span class=\"example-spacer\"></span>\n  <button mat-button [matMenuTriggerFor]=\"menu\">\n    <mat-icon>more_vert</mat-icon>\n  </button>\n  <mat-menu #menu=\"matMenu\">\n    <button mat-menu-item (click)=\"openAddContactDialog()\">About</button>\n    <button mat-menu-item (click)=\"toggleTheme.emit()\">Toggle theme</button>\n    <button mat-menu-item (click)=\"toggleDir.emit()\">Toggle dir</button>\n  </mat-menu>\n</mat-toolbar>\n"/* this was squashed */,
+  templateUrl: "./toolbar.component.html",
   styles: ['toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
@@ -43,20 +24,6 @@ export class ToolbarComponent implements OnInit {
 
   openAddContactDialog(): void {
     alert("???");
-    //let dialogRef = this.dialog.open(NewContactDialogComponent, {
-    //  width: '450px'
-    //});
-
-    //dialogRef.afterClosed().subscribe(result => {
-    //  console.log('The dialog was closed', result);
-
-    //  if (result) {
-    //    this.openSnackBar("Contact added", "Navigate")
-    //      .onAction().subscribe(() => {
-    //        this.router.navigate(['/contactmanager', result.id]);
-    //      });
-    //  }
-    //});
   }
 
   openSnackBar(message: string, action: string): MatSnackBarRef<SimpleSnackBar> {
