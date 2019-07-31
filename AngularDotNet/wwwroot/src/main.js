668f896cc0b1dd5d5bc5d93ca8497faa5847a58c
ddonovan <info@promatrixinc.com>
2019-07-31 11:22:46 -0500

    Removed map and js files to eliminate errors

--- AngularDotNet/wwwroot/src/main.js
+++
@@ -1,8 +0,0 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import "hammerjs";
-enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(function (err) { return console.error(err); });
-//# sourceMappingURL=main.js.map
