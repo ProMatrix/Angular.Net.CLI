815fd060977c55866749992f4e3efc27089d877a
ddonovan <info@promatrixinc.com>
2019-09-26 01:40:07 -0500

    ...

--- ProjectDebug/gulpfile.js
+++ ProjectDebug/gulpfile.js
@@ -4,56 +4,62 @@ const tl = require("./tasklist");
 const gulp = require("gulp");
 const net = require("net");
 
-gulp.task("print-time", complete => {
-    if (ct.getIsDebuggingGulp()) {
-        execute("print-time");
-    } else {
-        ct.printTime();
-    }
+gulp.task("print-time-d", complete => {
+    debug("print-time");
     complete();
 });
 
-gulp.task("print-version", complete => {
-    if (ct.getIsDebuggingGulp()) {
-        execute("print-version");
-    } else {
-        process.chdir("..\\AngularDotNet");
-        ct.printVersion();
-    }
+gulp.task("print-time-e", complete => {
+    ct.printTime();
     complete();
 });
 
-gulp.task("task-cofigure", complete => {
-    if (ct.getIsDebuggingGulp())
-        execute("task-cofigure");
-    else {
-        const t = require("../AngularDotNet/build_library/taskConfig");
-        new t.TaskConfig(true, "AngularDotNet");
-    }
+gulp.task("print-version-d", complete => {
+    process.chdir("..\\AngularDotNet");
+    debug("print-version");
     complete();
 });
 
-gulp.task("task-build", complete => {
-    if (ct.getIsDebuggingGulp())
-        execute("task-build");
-    else {
-        const t = require("../AngularDotNet/build_library/taskBuild");
-        new t.TaskBuild(true, "AngularDotNet", true);
-    }
+gulp.task("print-version-e", complete => {
+    process.chdir("..\\AngularDotNet");
+    ct.printVersion();
     complete();
 });
 
-gulp.task("task-launch", complete => {
-    if (ct.getIsDebuggingGulp()) {
-        execute("launch");
-    } else {
-        let t = require("../AngularDotNet/build_library/taskLaunch.js");
-        new l.TaskLaunch("AngularDotNet");
-    }
+gulp.task("task-cofig-d", complete => {
+    debug("task-cofigure");
     complete();
 });
 
-function execute(task) {
+gulp.task("task-cofig-e", complete => {
+    const t = require("../AngularDotNet/build_library/taskConfig");
+    new t.TaskConfig(true, "AngularDotNet");
+    complete();
+});
+
+gulp.task("task-build-d", complete => {
+    debug("task-build");
+    complete();
+});
+
+gulp.task("task-build-e", complete => {
+    const t = require("../AngularDotNet/build_library/taskBuild");
+    new t.TaskBuild(true, "AngularDotNet", true);
+    complete();
+});
+
+gulp.task("task-launch-d", complete => {
+    debug("launch");
+    complete();
+});
+
+gulp.task("task-launch-e", complete => {
+    let t = require("../AngularDotNet/build_library/taskLaunch.js");
+    new l.TaskLaunch("AngularDotNet");
+    complete();
+});
+
+function debug(task) {
     const client = new net.Socket();
     client.connect(1337, "127.0.0.1", function () { client.write(task); });
 }
