68371e5683c1f1c10c0267e237d8a4505d2a999b
ddonovan <info@promatrixinc.com>
2019-11-09 10:38:04 -0500

    Adding configure UI

--- AngularDotNet/wwwroot/projects/configure/src/app/app.component.ts
+++ AngularDotNet/wwwroot/projects/configure/src/app/app.component.ts
@@ -1,32 +1,102 @@
 import { Component } from '@angular/core';
-
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { MobileApisComponent } from '../../../../features/mobileApis.component';
+// services
+import { AppConfig } from '../../../../common/appConfig';
+import { EntityService } from '../../../../common/entityService';
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [AppConfig]
 })
 export class AppComponent {
-  title = 'configure';
+  private appTitle = 'Angular.Net Application (configure)';
+  private appHref: string;
+  private appCache: string;
+  private showOpeningTitle = true;
+  private showMobileApiView = false;
+  private selectedFeature: string;
+  private appLoaded = false;
+  private resizeTimerId: any;
+
+  constructor(private readonly route: ActivatedRoute, private readonly router: Router, private readonly ac: AppConfig) {
+    this.appHref = window.location.href;
+  }
+
+  ngAfterViewInit() {
+    this.ac.onResizeApp();
+    this.ac.onOrientationChange();
+
+    window.addEventListener('offline', (event: Event) => {
+      this.ac.toastrInfo('The application just went offline!');
+      this.ac.isOnline = false;
+    }, false);
+
+    window.addEventListener('online', (event: Event) => {
+      this.ac.toastrInfo('The application is back online!');
+      this.ac.isOnline = true;
+    }, false);
+
+    window.addEventListener('resize', (event: Event) => {
+      if (this.resizeTimerId) {
+        return;
+      }
+      this.resizeTimerId = setTimeout(() => {
+        this.ac.onResizeApp();
+        this.resizeTimerId = null;
+      }, 500);
+    }, false);
+
+    window.addEventListener('orientationchange', (event: Event) => {
+      setTimeout(() => {
+        this.ac.onOrientationChange();
+      });
+    }, false);
+
+    this.ac.getAppSettings(() => {
+      this.checkForUpdates();
+      this.navigateForward();
+    }, (errorMessage) => {
+      // this.toastr.warning(this.appTitle + ': is Offline!');
+      this.navigateForward();
+    });
+  }
+
+  private checkForUpdates() {
+    setTimeout(() => {
+      const versionNumber = this.ac.getLocalStorage('versionNumber');
+      if (versionNumber && versionNumber.vn !== this.ac.appSettings.buildVersion && !this.ac.appSettings.debug) {
+        this.ac.setLocalStorage('versionNumber', { vn: this.ac.appSettings.buildVersion });
+        // this.toastr.info('A newer version is available! Restarting the application...');
+        setTimeout(() => {
+          this.restartApp();
+        }, 5000);
+      } else {
+        this.ac.setLocalStorage('versionNumber', { vn: this.ac.appSettings.buildVersion });
+        setTimeout(() => {
+          if (navigator.onLine) {
+            this.ac.isOnline = true;
+            // this.toastr.success('This application is operating online as normal.', 'Success!');
+          } else {
+            this.ac.isOnline = false;
+            // this.toastr.warning('This application is operating offline as normal.', 'Warning!');
+          }
+        });
+      }
+    }, 3000);
+  }
+
+  private navigateForward() {
+    setTimeout(() => {
+      this.showOpeningTitle = false;
+      this.showMobileApiView = true;
+      this.router.navigate(['/mobileApis']);
+    }, this.ac.appSettings.splashTime); // navigate away from splash view
+  }
+
+  private restartApp() {
+    window.location.href = this.appHref;
+  }
+
 }
