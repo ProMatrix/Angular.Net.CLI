3a7d756d9044ced3471d13dfa80ca9b4cc0c2510
ddonovan <info@promatrixinc.com>
2019-11-22 15:46:08 -0500

    ...

---
+++ ProjectDebug/git-hooks/pre-commit.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var taskGitCommit_1 = require("../../angularnetcore/build_library/taskGitCommit");
+var PreCommit = /** @class */ (function () {
+    function PreCommit() {
+        try {
+            process.chdir('./AngularNetCore');
+            new taskGitCommit_1.TaskGitCommit(false, 'AngularNetCore', true);
+            //throw new Error('Debug Message!');
+        }
+        catch (e) {
+            console.error(e.message);
+            process.exit(1);
+        }
+    }
+    return PreCommit;
+}());
+exports.PreCommit = PreCommit;
+new PreCommit();
+//# sourceMappingURL=pre-commit.js.map
