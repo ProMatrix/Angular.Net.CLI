fd7ea361d49413bc4e6613148aa8d225ec8ebf46
ddonovan <info@promatrixinc.com>
2019-11-22 11:19:59 -0500

    .

--- print-in-frame.js
+++ print-in-frame.js
@@ -1,5 +1,13 @@
 // In print-in-frame.js
 
 const printInFrame = (size, header) => {
+    console.log('*'.repeat(size));
+    console.log(header);
+    console.log('*'.repeat(size));
+};
 
+if (require.main === module) {
+    printInFrame(process.argv[2], process.argv[3]);
+} else {
+    module.exports = printInFrame;
 }
