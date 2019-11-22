d4dee94bf8d30228837e66043e977bac1c1d1985
ddonovan <info@promatrixinc.com>
2019-11-22 15:44:55 -0500

    .

--- AngularNetCore/git_hooks/pre-commit.js
+++ AngularNetCore/git_hooks/pre-commit.js
@@ -1,7 +1,15 @@
 var PreCommit = /** @class */ (function () {
     function PreCommit() {
+        try {
+            throw new Error("!!!");
+        }
+        catch (e) {
+            console.error(e.message);
+            process.exit(1);
+        }
     }
     return PreCommit;
 }());
 export { PreCommit };
+new PreCommit();
 //# sourceMappingURL=pre-commit.js.map
--- AngularNetCore/git_hooks/pre-commit.ts
+++ AngularNetCore/git_hooks/pre-commit.ts
@@ -1,6 +1,12 @@
-
-
 export class PreCommit {
 
-
+    constructor() {
+        try {
+            throw new Error("!!!");
+        } catch (e) {
+            console.error(e.message);
+            process.exit(1);
+        }
+    }
 }
+new PreCommit();
