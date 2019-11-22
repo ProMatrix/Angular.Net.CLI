203cd5d58a4f9ac9974988a5076e6b07779cda67
ddonovan <info@promatrixinc.com>
2019-11-21 20:01:30 -0500

    ...

--- AngularNetCore/git_hooks/pre-commit.js
+++ AngularNetCore/git_hooks/pre-commit.js
@@ -1,20 +1,18 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var taskGitCommit_1 = require("../taskGitCommit");
 var PreCommit = /** @class */ (function () {
     function PreCommit() {
         try {
-            var tgh = new taskGitCommit_1.TaskGitCommit();
-            process.chdir("../Angular.Net.CLI/ProjectBuild");
-            tgh.execute();
+            throw new Error("ISSUES");
+            //const tgh = new TaskGitCommit();
+            //process.chdir("../Angular.Net.CLI/ProjectBuild");
+            //tgh.execute();
         }
         catch (e) {
             console.error(e.message);
-            process.exit(1);
+            //process.exit(1);
         }
     }
     return PreCommit;
 }());
-exports.PreCommit = PreCommit;
+export { PreCommit };
 new PreCommit();
 //# sourceMappingURL=pre-commit.js.map
