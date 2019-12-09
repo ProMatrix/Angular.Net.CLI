c34ebfc1f01fbcd74b72ddbe0e38d5a3a7b248fc
ddonovan <info@promatrixinc.com>
2019-12-09 14:40:59 -0500

    ...

--- ProjectDebug/git-hooks/pre-push.js
+++ ProjectDebug/git-hooks/pre-push.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var taskGitMerge_1 = require("../../angularnetcore/build_library/taskGitMerge");
 var PrePush = /** @class */ (function () {
     function PrePush() {
         try {
+            new taskGitMerge_1.TaskGitMerge('master', 'npm');
+            // we can only see the console.log is the process.exit(1);
+            // process.exit(1);
         }
         catch (e) {
             console.error(e.message);
--- ProjectDebug/git-hooks/pre-push.ts
+++ ProjectDebug/git-hooks/pre-push.ts
@@ -1,8 +1,12 @@
+import { TaskGitMerge } from '../../angularnetcore/build_library/taskGitMerge';
 export class PrePush {
 
     constructor() {
         try {
+            new TaskGitMerge('master', 'npm');
 
+            // we can only see the console.log is the process.exit(1);
+            // process.exit(1);
         } catch (e) {
             console.error(e.message);
             process.exit(1);
