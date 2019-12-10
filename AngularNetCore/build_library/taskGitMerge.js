80979d7854dfe47ab8995c9ec376cb7a793d3d44
ddonovan <info@promatrixinc.com>
2019-12-10 02:33:27 -0500

    ...

--- AngularNetCore/build_library/taskGitMerge.js
+++ AngularNetCore/build_library/taskGitMerge.js
@@ -49,7 +49,7 @@ var TaskGitMerge = /** @class */ (function (_super) {
     }
     TaskGitMerge.prototype.execute = function () {
         var outgoingMerges = this.cli.executeSync('git log ' + this.mergeTo + ' --merges --not --remotes');
-        console.log('outgoingMerges: ' + outgoingMerges);
+        console.log('outgoingMerges (' + this.mergeFrom + ' -> ' + this.mergeTo + '): ' + outgoingMerges);
         if (outgoingMerges.length > 0) {
             // any merges into the mergeTo branch will publish to npm
             process.chdir('angular-lib');
