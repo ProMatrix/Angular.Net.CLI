d2814f4c059e8381b1cd0ae2ed416cd54258ff57
ddonovan <info@promatrixinc.com>
2019-12-10 08:41:37 -0500

    ...

--- ProjectDebug/git-hooks/pre-push.ts
+++ ProjectDebug/git-hooks/pre-push.ts
@@ -5,12 +5,20 @@ export class PrePush {
 
     constructor() {
         try {
+            // assume the libary_ng is in wwwroot, find the path for the npm update
+
+            const cwd = process.cwd();
+            process.chdir('../');
+            const wwwroot = process.cwd();
+            process.chdir(cwd);
             const tgm = new TaskGitMerge('master', 'npm');
+
+            process.chdir(wwwroot);
             if (tgm.mergeCompleted) {
                 new TaskNpmUpdate('ng2-express');
             }
             // we can only see the console.log is the process.exit(1);
-             process.exit(1);
+            // process.exit(1);
         } catch (e) {
             console.error(e.message);
             process.exit(1);
