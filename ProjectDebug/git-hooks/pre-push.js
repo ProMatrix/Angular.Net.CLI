54543d1257afe71d97b4ff3425c27ee034904df8
ddonovan <info@promatrixinc.com>
2019-12-24 11:10:11 -0500

    ...

--- ProjectDebug/git-hooks/pre-push.js
+++ ProjectDebug/git-hooks/pre-push.js
@@ -1,18 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var taskGitPush_1 = require("../../angularnetcore/build_library/taskGitPush");
-var taskNpmUpdate_1 = require("../../angularnetcore/build_library/taskNpmUpdate");
+var taskNpmPublish_1 = require("../../angularNetCore/build_library/taskNpmPublish");
 var PrePush = /** @class */ (function () {
     function PrePush() {
         try {
             // assume the libary_ng is in wwwroot, find the path for the npm update
-            var tgp = new taskGitPush_1.TaskGitPush('ng2-express', 'npm');
-            //process.chdir('..\\'); // quick
-            process.chdir('..\\..\\');
-            // should now be in wwwroot
-            if (tgp.publishCompleted) {
-                new taskNpmUpdate_1.TaskNpmUpdate('ng2-express');
-            }
+            var noop = new taskNpmPublish_1.TaskNpmPublish("ng2-express", "npm");
             // we can only see the console.log is the process.exit(1);
             // process.exit(1);
         }
--- ProjectDebug/git-hooks/pre-push.ts
+++ ProjectDebug/git-hooks/pre-push.ts
@@ -1,19 +1,11 @@
-import { TaskGitPush } from '../../angularnetcore/build_library/taskGitPush';
-import { TaskNpmUpdate } from '../../angularnetcore/build_library/taskNpmUpdate';
+import { TaskNpmPublish } from "../../angularNetCore/build_library/taskNpmPublish";
 
 export class PrePush {
 
     constructor() {
         try {
             // assume the libary_ng is in wwwroot, find the path for the npm update
-            const tgp = new TaskGitPush('ng2-express', 'npm');
-            //process.chdir('..\\'); // quick
-
-            process.chdir('..\\..\\');
-            // should now be in wwwroot
-            if (tgp.publishCompleted) {
-                new TaskNpmUpdate('ng2-express');
-            }
+            const noop = new TaskNpmPublish("ng2-express", "npm");
             // we can only see the console.log is the process.exit(1);
             // process.exit(1);
         } catch (e) {
