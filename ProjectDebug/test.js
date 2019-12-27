62a1f5fef602a1d9d0fac6736139bfe9b6885f3e
ddonovan <info@promatrixinc.com>
2019-12-27 06:21:14 -0500

    ...

---
+++ ProjectDebug/test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('HEY!');
+while (true) {
+}
+//# sourceMappingURL=test.js.map
---
+++ ProjectDebug/test.ts
@@ -0,0 +1,4 @@
+import { ExpressModule } from 'self-control';
+console.log('HEY!');
+while (true) {
+}
