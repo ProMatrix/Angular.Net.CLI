d1fc7d16a5c68a6fd69ba368e590f7a0ec91b938
ddonovan <info@promatrixinc.com>
2019-12-30 18:39:29 -0500

    ...

--- AngularNetCore/gulpfile.js
+++ AngularNetCore/gulpfile.js
@@ -1,4 +1,4 @@
 const gulp = require("gulp");
 const cwd = process.cwd();
 const vsProjectName = cwd.substr(cwd.lastIndexOf("\\") + 1);
-require("./node_modules/self-control/src/gulpTasks")(gulp, vsProjectName);
+require("self-control/src/gulpTasks")(gulp, vsProjectName);
