7d50d1db2a197e11a7ac0c04b06d6579330b8e86
ddonovan <info@promatrixinc.com>
2020-02-02 14:14:52 -0500

    ...

--- AngularNetCore/wwwroot/projects/template/src/app/template.component.ts
+++ AngularNetCore/wwwroot/projects/template/src/app/template.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, AfterViewChecked, AfterViewInit, EventEmitter, Outpu
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 // services
 import { AppConfig } from '../../../../common/appConfig';
-import { TimingMetrics } from 'ngx-modeling';
-
 
 @Component({
     // #region template
